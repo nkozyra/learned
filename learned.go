3e7c9659543372aeebdef4cd66cd258e33e13642
Nathan Kozyra <nkozyra@gmail.com>
2016-04-01 21:09:39 -0400

    init

---
+++ learned.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/nkozyra/learned/src"
+	"log"
+)
+
+func main() {
+	k := learned.NewKNN()
+	log.Println(k)
+}
---
+++ src/knn.go
@@ -0,0 +1,19 @@
+package learned
+
+type KNN struct {
+	xData []float64
+	yData []float64
+}
+
+func NewKNN() (k KNN) {
+
+	return k
+}
+
+func (k KNN) AddTest() {
+
+}
+
+func (k KNN) Query() {
+
+}
